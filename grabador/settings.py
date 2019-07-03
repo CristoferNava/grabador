8833db43461d6485123e574acced22ae7131d65b
cristofer <cj.carmonanava@ugto.mx>
2019-07-03 10:48:44 -0500

    It was added support for a local database

--- grabador/settings.py
+++ grabador/settings.py
@@ -54,12 +54,8 @@ WSGI_APPLICATION = 'grabador.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'grabadordb',
-        'USER': 'cristofer',
-        'PASSWORD': 'julie1998',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
