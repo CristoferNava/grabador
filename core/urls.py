f1ef01b2d698f2d9e97fbb32d1afcde2a61cad15
cristofer <cj.carmonanava@ugto.mx>
2019-07-05 09:59:49 -0500

    The url web was added

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views as core_views
 
 urlpatterns = [
     path('', core_views.home, name='home'),
+    path('web/', core_views.home, name='web'),
     path('about/', core_views.about, name='about'),
     path('points/', core_views.points, name='points'),
     path('store/', core_views.store, name='store'),
