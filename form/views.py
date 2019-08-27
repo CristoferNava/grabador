321ef16502c1e5e91a0a8ea3440d5ac5e2ba3e59
cristofer <cj.carmonanava@ugto.mx>
2019-08-27 13:38:21 -0500

    Pre-server

--- form/views.py
+++ form/views.py
@@ -1,3 +1,5 @@
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from form.forms import ContactForm
@@ -9,6 +11,7 @@ class Form(TemplateView):
         form = ContactForm
         return render(request, self.template_name, {'form': form})
 
+    '''
     def post(self, request):
         form = ContactForm(request.POST)
 
@@ -20,3 +23,27 @@ class Form(TemplateView):
             }
 
             return render(request, self.template_name, args)
+    '''
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        text = request.POST.get('text')
+
+        body = render_to_string(
+            'form/email_content.html', {
+                'name': name,
+                'email': email,
+                'text': text,
+            },
+        )
+
+        email_message = EmailMessage(
+            subject = 'Mensaje de usuario',
+            body = body,
+            from_email=email,
+            to=['cj.carmonanava@utgo.mx']
+        )
+        email_message.content_subtype = 'html'
+        email_message.send()
+    
